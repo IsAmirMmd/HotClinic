d775b64499da0fd0b0327a834164d1b94a7dca1f
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-05-31 00:26:20 +0330

    doctors firstLoad function added and prescriptions updated

--- src/main/java/org/clinic/Doctor.java
+++ src/main/java/org/clinic/Doctor.java
@@ -1,5 +1,6 @@
 package org.clinic;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 
 public class Doctor extends Person {
@@ -35,18 +36,36 @@ public class Doctor extends Person {
         return null;
     }
 
-    public Patient.Prescription writePrescription(Patient patient, Drug medication) {
+    public Patient.Prescription writePrescription(Patient patient, ArrayList<Drug> medication) {
         if (patients.contains(patient)) {
-            medication.setQuantity(medication.getQuantity() - 1);
-            if (medication.getQuantity() == 0) {
-                System.out.println("no more of " + medication.getName() + "!");
-            } else {
-                Patient.Prescription prescription = new Patient.Prescription(new java.util.Date(), medication.getName(),
-                        this, patient);
-                patient.addPrescription(prescription);
-                return prescription;
+            for (Drug drug : medication) {
+                if (drug.getQuantity() == 0) {
+                    medication.remove(drug);
+                } else {
+                    drug.setQuantity(drug.getQuantity() - 1);
+                }
             }
+            Patient.Prescription prescription = new Patient.Prescription(new java.util.Date(), medication, this, patient);
+            for (Drug drug : medication) {
+                patient.addMedication(drug.getName());
+            }
+            patient.addPrescription(prescription);
+            return prescription;
         }
         return null;
     }
+
+    public static ArrayList<Doctor> firstLoad() {
+        ArrayList<Doctor> doctors = new ArrayList<>();
+
+        doctors.add(new Doctor("Dr. Ali Mohammadi", "Tehran, Valiasr Street", "09123456789", "Headache"));
+        doctors.add(new Doctor("Dr. Sara Rahimi", "Isfahan, Chaharbagh Street", "09123456789", "Anemia"));
+        doctors.add(new Doctor("Dr. Mohammad Hosseini", "Shiraz, Hafez Street", "09123456789", "Allergy"));
+        doctors.add(new Doctor("Dr. Maryam Farahani", "Tabriz, Saat Square", "09123456789", "High blood pressure"));
+        doctors.add(new Doctor("Dr. Hossein Zarei", "Mashhad, Imam Reza Street", "09123456789", "Sinusitis"));
+        doctors.add(new Doctor("Dr. Zahra Gholami", "Tehran, Enghelab Street", "09123456789", "Diabetes"));
+
+        return doctors;
+    }
+
 }
