5f133d658c6cac1de5c407f576aff4ff3bfd290c
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-05-31 00:28:48 +0330

    Nurse accessibility updated

--- src/main/java/org/clinic/Nurse.java
+++ src/main/java/org/clinic/Nurse.java
@@ -3,11 +3,11 @@ package org.clinic;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Nurse extends Person{
+public class Nurse extends Person {
     private ArrayList<Patient> patients;
 
-    public Nurse(String name,String address,String phone) {
-        super(name,address,phone);
+    public Nurse(String name, String address, String phone) {
+        super(name, address, phone);
         this.patients = new ArrayList<>();
     }
 
@@ -29,5 +29,16 @@ public class Nurse extends Person{
             return patient.getPrescriptions();
         }
         return null;
+    }
+
+    public static ArrayList<Nurse> firstLoad() {
+        ArrayList<Nurse> nurses = new ArrayList<>();
+
+        nurses.add(new Nurse("Maryam Ebrahimi", "Tehran, Valiasr Street", "09123456789"));
+        nurses.add(new Nurse("Ali Mohammadi", "Isfahan, Chaharbagh Street", "09123456789"));
+        nurses.add(new Nurse("Sara Hosseini", "Shiraz, Hafez Street", "09123456789"));
+        nurses.add(new Nurse("Hossein Ahmadi", "Tabriz, Saat Square", "09123456789"));
+
+        return nurses;
     }
 }
