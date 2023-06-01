345cc48d1be88d2b2131b1d8a397eed361a495eb
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-06-01 17:44:20 +0330

    personnel class completed

--- src/main/java/org/clinic/Personnel.java
+++ src/main/java/org/clinic/Personnel.java
@@ -1,4 +1,29 @@
 package org.clinic;
 
-//public class Personnel extends Person{
-//}
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Personnel extends Person {
+    private String task;
+
+    public Personnel(String name, String address, String phone, long salary, String task) {
+        super(name, address, phone, salary);
+        this.task = task;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public static ArrayList<Personnel> firstLoad() {
+        ArrayList<Personnel> personnels = new ArrayList<>();
+        personnels.add(new Personnel("asghar", "mashhad", "09152547856", 1000000, "WC"));
+        return personnels;
+    }
+
+}
+
